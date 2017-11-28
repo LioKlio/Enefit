2f2c6290c07e83f1a04b88c43a015ed8eb0e6dea
lioklio <liona.tomm@gmail.com>
2017-11-28 23:00:11 +0200

    ups

--- js/main.js
+++ js/main.js
@@ -1,14 +1,31 @@
 $(window).scroll(function(){
   var win_scroll = $(window).scrollTop()
-  if (win_scroll == 0 || win_scroll < 200) $('body').removeClass('__fixed')
-  else $('body').addClass('__fixed');
-  // if (win_scroll > 800) $('.scrolltop').fadeIn(400)
-  // else $('.scrolltop').fadeOut(800);
+  if (win_scroll == 0 || win_scroll < 200) $('body').removeClass('fixed')
+  else {
+    $('body').addClass('fixed')
+    $('.show-nav').removeClass('show-nav')
+  };
 })
 
-// $('body').click(function(e){
-//   if($(e.target).hasClass('opened')) $('.opened').removeClass('opened')
-// })
-// $('.search, .langs').click(function(){
-//   if( !$(this).hasClass('opened') ) $(this).addClass('opened');
-// })
+$('body').click(function(e){
+  if($(e.target).closest('.navigation').hasClass('navigation')) return 1;
+
+  var elemShow = $(e.target).closest('.show')
+  if(elemShow.hasClass('mobile-toggle')) $('.header').addClass('active');
+  if(elemShow.hasClass('opened')){
+    if(elemShow.hasClass('search')) return 1;
+    if(elemShow.hasClass('mobile-toggle')) $('.header').removeClass('active');
+    elemShow.removeClass('opened')
+  } else {
+    $('.opened').removeClass('opened')
+    $('.show-nav').removeClass('show-nav')
+    if(!elemShow.hasClass('mobile-toggle')) $('.header').removeClass('active')
+    elemShow.addClass('opened')
+  }
+})
+$('.subnav').click(function(){ $('.subnav ul').toggleClass('nav-opened') })
+
+$('.navigation li.curr a').click(function(e){
+  e.preventDefault()
+  $(this).closest('.navigation').addClass('show-nav')
+})
